5d339aff01ebf8ebdc4f95d91f084df1ff376446
sarah khalil <mkhalil.sarah@gmail.com>
2016-02-01 11:35:41 +0100

    [WIP] Added contact form

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ContactController
+{
+    /**
+     * @Template
+     */
+    public function contactAction()
+    {
+        return array();
+    }
+}
